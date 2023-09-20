8477bc2fdac0563d5e3550812dfbaaeaf7f1509c
Joshua Wiser <Jwiser53@byui.edu>
2023-09-19 21:46:15 -0600

    finished calculations

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -18,5 +18,26 @@ class Program
                 numbers.Add(inputNumber);
             }
         }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine("The sum is: " + sum);
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine("The average is: " + average);
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine("The max is: " + max);
     }
 }
