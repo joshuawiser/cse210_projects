34656559aa820d2123932390fb450120cedb61d4
Joshua Wiser <Jwiser53@byui.edu>
2023-09-19 21:25:13 -0600

    made guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int secretNumber = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+
+        while (guess != secretNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (secretNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (secretNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
