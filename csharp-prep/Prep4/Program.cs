b9eac74cf0f35124db50063f0fcf977b0251d7c7
Joshua Wiser <Jwiser53@byui.edu>
2023-09-19 21:37:51 -0600

    created the loop to collect the numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int inputNumber = -1;
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter a positive number (0 to quit): ");
+            string userResponse = Console.ReadLine();
+            inputNumber = int.Parse(userResponse);
+
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        }
     }
 }
