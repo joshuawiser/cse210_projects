c2d06760a96b0976429daac431210443825423eb
Joshua Wiser <Jwiser53@byui.edu>
2023-09-27 20:14:40 -0600

    finished learning2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Credit Consultant";
+        job1._company = "Progrexion";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Team Lead";
+        job2._company = "Squeeze Media";
+        job2._startYear = 2023;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Joshua Wiser";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
