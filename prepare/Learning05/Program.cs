47af30709794e46ba89b3208fccf6a657868b533
Joshua Wiser <Jwiser53@byui.edu>
2023-11-09 19:43:46 -0700

    learning5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Orange", 7);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Green", 6, 4);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Blue", 12);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
